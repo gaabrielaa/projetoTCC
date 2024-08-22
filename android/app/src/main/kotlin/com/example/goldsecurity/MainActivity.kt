a3eedf3c773117737331035e1040b7f95450ab0f
Gabriella <gabriella.novaes@portalsesisp.org.br>
2024-08-22 08:00:42 -0300

    inicio do projeto

---
+++ android/app/src/main/kotlin/com/example/goldsecurity/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.goldsecurity
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:goldsecurity/Telas/Login/login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Login(),
+  ));
+}
+
